4e74de73a1c0f52ed5ea18935d73f407bf521f78
Wasper <Wasper256@users.noreply.github.com>
2016-05-27 16:13:43 +0300

    Add files via upload
    Added function for downloading one image from post. 
    Added some commenst to code.

--- vk_get.py
+++ vk_get.py
@@ -1,22 +1,26 @@
 import json
+import urllib.request
 from urllib.request import urlopen
 
+# get data from vk.com
 address = 'https://api.vk.com/method/wall.get?owner_id=-100567023&count=10'
 data = urlopen(address)
 decoded_response = data.read().decode()
 final_data = json.loads(decoded_response)
 
+# formattig response
 response1 = final_data['response'][1:2]
 respid = str(response1)
 respid = respid.split()
 
+# getting last id post
 position = respid.index("'id':")
 idp = (respid[position + 1])
 idp = idp.replace(',', '')
 idp = idp.replace('}', '')
 idp = idp.replace(']', '')
-print(idp)
 
+# getting previous id and writing new id
 filename = '/home/wasper/Documents/Python/Projects/reposter/vk_check/last_id.txt'
 pid = open(filename, 'r')
 pid = pid.read()
@@ -27,11 +31,12 @@ file.close()
 idp = int(idp)
 pid = int(pid)
 rp = (response1)
-if(idp - pid == 1):
+# getting post
+if(idp - pid != 0):
+    # getting text of last post
     rp = str(rp)
     rp = rp.split("'")
-    pos = rp.index('copy_text')
-    print(pos)
+    pos = rp.index('text')
     text = (rp[pos + 2])
     print(text)
     fn = '/home/wasper/Documents/Python/Projects/reposter/vk_check/%s.txt' % (idp)
@@ -39,5 +44,11 @@ if(idp - pid == 1):
     file.write(text)
     file.close()
 
+    # getting image
+    c = rp.count('src_big')  # test for image
+    if(c > 0):
+        pos = rp.index('src_big')
+        photol = (rp[pos + 2])
+        urllib.request.urlretrieve("%s" % (photol), "/home/wasper/Documents/Python/Projects/reposter/vk_check/%s.jpg" % (idp))
 else:
     print('no new posts')
