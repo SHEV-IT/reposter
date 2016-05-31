8cfe1fb9d3674383b3cdb6f1f79e4b671e335ad4
Wasper <Wasper256@users.noreply.github.com>
2016-05-31 18:52:28 +0300

    Update vk_get.py
    Gettig id and text directly from json object.
    Fixed "%s" to .format

--- vk_get.py
+++ vk_get.py
@@ -1,49 +1,39 @@
 import json
-import re
 import urllib.request
 from urllib.request import urlopen
 
 # get data from vk.com
-address = 'https://api.vk.com/method/wall.get?owner_id=-100567023&count=10'
+address = 'https://api.vk.com/method/wall.get?owner_id=-16092359&count=10'
 data = urlopen(address)
 decoded_response = data.read().decode()
 final_data = json.loads(decoded_response)
 
-# formattig response
-response1 = final_data['response'][1:2]
-respid = str(response1)
-respid = respid.split()
-
-# getting last id post
-position = respid.index("'id':")
-idp = (respid[position + 1])
-idp = re.sub(r'\,|\}|\]', '', idp)
+# getting last post response
+response = final_data['response'][1:2]
+for resp in response:
+    current_id = resp['id']  # getting an id
+    text = resp['text']  # getting txt
 
 # getting previous id and writing new id
 with open('last_id.txt') as f:
-    pid = f.read()
+    prev_id = f.read()
 with open('last_id.txt', 'w') as f:
-    f.write(idp)
+    current_id = str(current_id)  # it must be string
+    f.write(current_id)
+current_id = int(current_id)  # and now it must be integer
+prev_id = int(prev_id)
 
-idp = int(idp)
-pid = int(pid)
-rp = (response1)
 # getting post
-if(idp - pid != 0):
-    # getting text of last post
-    rp = str(rp)
-    rp = rp.split("'")
-    pos = rp.index('text')
-    text = (rp[pos + 2])
-    print(text)
-    with('%s.txt' % (idp), 'w') as w:
-        w.write(text)
-
+if(current_id - prev_id != 0):
+    with open('{0}.txt'.format(current_id), 'w') as t:
+        t.write(text)
     # getting image
+    rp = str(resp)
+    rp = rp.split()
     c = rp.count('src_big')  # test for image
     if(c > 0):
-        pos = rp.index('src_big')
-        photol = (rp[pos + 2])
-        urllib.request.urlretrieve("%s" % (photol), "%s.jpg" % (idp))
+        pos = rp.index('src_big')  # getting image url
+        url = (rp[pos + 2])
+        urllib.request.urlretrieve("{0}".format(url), "{0}.jpg".format(current_id))  # writing image
 else:
     print('no new posts')
