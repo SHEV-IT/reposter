c1d700908f0f9b9c49221dfd5d1973379ec5dea3
Wasper <Wasper256@users.noreply.github.com>
2016-05-31 18:19:10 +0300

    Update telebot.py
    change "%s" to .format

--- telebot.py
+++ telebot.py
@@ -7,12 +7,12 @@ chat_id = "<here your chat id>"
 with open('last_id.txt') as f:
     lid = f.read()
 # reading last post txt
-with open('%s.txt' % (lid)) as t:
+with open('{0}.txt'.format(lid)) as t:
     text = t.read()
 # sending text
-url_send = "https://api.telegram.org/bot%s/sendmessage?chat_id=%s&text=%s" % (token, chat_id, text)
+url_send = "https://api.telegram.org/bot{0}/sendmessage?chat_id={1}&text={2}".format(token, chat_id, text)
 urlopen(url_send)
 # sending image
 data = {'chat_id': chat_id}
-files = {'photo': open("%s.jpg" % (lid), "rb")}
-requests.post("https://api.telegram.org/bot%s" % (token) + '/sendPhoto', data=data, files=files)
+files = {'photo': open("{0}.jpg".format(lid), "rb")}
+requests.post("https://api.telegram.org/bot{0}s".format(token) + '/sendPhoto', data=data, files=files)
