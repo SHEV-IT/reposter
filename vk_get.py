03b4c08feb79ec0979fd261ab30f7804f689eae2
Wasper <Wasper256@users.noreply.github.com>
2016-06-03 18:56:59 +0300

    fix
    added launch telegram bot in case of new post. fixed problem with image.

--- vk_get.py
+++ vk_get.py
@@ -1,9 +1,11 @@
+import os
 import json
+import re
 import urllib.request
 from urllib.request import urlopen
 
 # get data from vk.com
-address = 'https://api.vk.com/method/wall.get?owner_id=-16092359&count=10'
+address = 'https://api.vk.com/method/wall.get?owner_id=<here owner id(personal_id or group)>&count=5'
 data = urlopen(address)
 decoded_response = data.read().decode()
 final_data = json.loads(decoded_response)
@@ -13,7 +15,6 @@ response = final_data['response'][1:2]
 for resp in response:
     current_id = resp['id']  # getting an id
     text = resp['text']  # getting txt
-
 # getting previous id and writing new id
 with open('last_id.txt') as f:
     prev_id = f.read()
@@ -24,16 +25,19 @@ current_id = int(current_id)  # and now it must be integer
 prev_id = int(prev_id)
 
 # getting post
-if(current_id - prev_id != 0):
+if(current_id - prev_id == 0):
     with open('{0}.txt'.format(current_id), 'w') as t:
         t.write(text)
+
     # getting image
     rp = str(resp)
     rp = rp.split()
-    c = rp.count('src_big')  # test for image
+    c = rp.count("'src_big':")  # test for image
     if(c > 0):
-        pos = rp.index('src_big')  # getting image url
-        url = (rp[pos + 2])
+        pos = rp.index("'src_big':")  # getting image url
+        url = (rp[pos + 1])
+        url = re.sub(r"\'|\,", '', url)
         urllib.request.urlretrieve("{0}".format(url), "{0}.jpg".format(current_id))  # writing image
+    os.system('python3 telebot.py')
 else:
     print('no new posts')
