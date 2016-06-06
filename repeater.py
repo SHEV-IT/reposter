459db8ce54b1b8612a8bb09b18e7711b6a453206
Wasper <Wasper256@users.noreply.github.com>
2016-06-06 22:11:46 +0300

    Delete repeater.py

--- repeater.py
+++
@@ -1,7 +0,0 @@
-import threading
-import os
-# repeater for our script. check for updates every 5 min
-def repeater():
-    threading.Timer(300.0, repeater).start()
-    os.system('python3 vk_get.py')
-repeater()
